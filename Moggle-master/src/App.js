31b8f16f47569fc396ec7290b0b5fea93b6c4737
deepanshi710 <66430219+deepanshi710@users.noreply.github.com>
2022-04-13 14:43:50 +0530

    Add files via upload

---
+++ Moggle-master/src/App.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Routes from "./Components/Routes";
+
+
+function App() {
+  const [darkTheme, setDarkTheme] = useState (false);
+  return (
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
+        <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
